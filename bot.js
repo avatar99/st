3fe2ab5edc0235177ceaaafb1fd1f1b4d932c2c4
avatar99 <51578188+avatar99@users.noreply.github.com>
2019-07-20 22:56:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,42 +1,45 @@
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = ".";
-var adminprefix = '.'
 
-
-const developers = ["600369127907131412"]
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'sg')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'sw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'sl')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/dream");
-      message.channel.send(`**✅**`)
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setava')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
+client.on('ready', () => {
+	console.log('Dream iS Here');
 });
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`b 1.0`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+    const dreamv2 = ["600369127907131412"]
+    client.on('message', message => {
+        var dream = message.content.split(` `).slice(1).join(' ');
+          if (!dreamv2.includes(message.author.id)) return;
+          if(message.content.startsWith(prefix + "sg")){
+            client.user.setActivity(dream);
+            message.channel.send(`✔️ **Done Playing Now __${dream}__**`)
+        }
+          if (message.content.startsWith(prefix + 'sw')) {
+      client.user.setActivity(dream, {type:'WATCHING'});
+          message.channel.send(`✔️  **Done Watching Now __${dream}__ **`)
+      } else
+      if (message.content.startsWith(prefix + 'sl')) {
+      client.user.setActivity(dream , {type:'LISTENING'});
+          message.channel.send(`✔️ **Done Listening Now __${dream}__** `)
+      } else
+      if (message.content.startsWith(prefix + 'st')) {
+    client.user.setGame(dream, "https://www.twitch.tv/dream");
+         message.channel.send(`✔️ **Done Stream Now  __${dream}__ **`)
+      }
+ 
+    });
 
 
 client.login('NjAwMzY5MTI3OTA3MTMxNDEy.XTNXmg.GO67n3NipjRI2VTkxxgO096UGJc');//
